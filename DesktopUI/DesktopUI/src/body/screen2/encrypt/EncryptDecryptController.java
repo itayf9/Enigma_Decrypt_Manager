a78ea50e76c51f13e0ac8c4db9c44d54a53ed45e
Danielsio <kingddd301@gmail.com>
2022-08-26 15:06:56 +0300

    fixed idea

--- DesktopUI/DesktopUI/src/body/screen2/encrypt/EncryptDecryptController.java
+++ DesktopUI/DesktopUI/src/body/screen2/encrypt/EncryptDecryptController.java
@@ -36,7 +36,6 @@ public class EncryptDecryptController {
             outputLabel.setText(outputLabel.getText() + cipheredCharStatus.getCipheredText());
             cipherProblemLabel.setText("");
         }
-
     }
 
     /**
