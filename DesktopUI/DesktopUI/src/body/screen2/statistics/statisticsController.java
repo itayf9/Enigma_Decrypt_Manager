1d2f10e98e8d65bbe3297c86dddf1091a536e05f
Danielsio <kingddd301@gmail.com>
2022-08-26 16:21:12 +0300

    added statistics

---
+++ DesktopUI/DesktopUI/src/body/screen2/statistics/statisticsController.java
@@ -0,0 +1,11 @@
+package body.screen2.statistics;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class statisticsController {
+
+    @FXML
+    private VBox vBoxHistory;
+
+}
